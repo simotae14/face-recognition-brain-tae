ec339ff9a894febf051f44eab6faf57c83adff6f
Simone Taeggi <startae14@gmail.com>
2020-05-16 16:34:27 +0200

    add rank component and the particles library for the animated background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 800,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 800,
+        size: 80,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/*
+    <Particles
+      className='particles'
+      params={particlesOptions} />
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
+    {/*
       <FaceRecognition />*/}
     </div>
   );
